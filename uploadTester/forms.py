722644a9fe9ed200041b5a5bd7562f9e635622c0
Blorenz <blorenz@gmail.com>
2014-03-14 15:37:21 -0400

    Go

--- uploadTester/forms.py
+++ uploadTester/forms.py
@@ -3,3 +3,4 @@ from django import forms
 
 class UploadForm(forms.Form):
     time = forms.CharField(max_length=20)
+    size = forms.CharField(max_length=20)
--- uploadTester/views.py
+++ uploadTester/views.py
@@ -1,17 +1,45 @@
+from __future__ import division
 from django.shortcuts import render, redirect, HttpResponse
 from .forms import UploadForm
 from django.views.decorators.csrf import csrf_exempt
 
+
+def humanize_bytes(bytes, precision=1):
+    """Return a humanized string representation of a number of bytes.
+
+    Assumes `from __future__ import division`.
+
+
+    '1.3 GB'
+    """
+    abbrevs = (
+        (1<<50L, 'PB'),
+        (1<<40L, 'TB'),
+        (1<<30L, 'GB'),
+        (1<<20L, 'MB'),
+        (1<<10L, 'kB'),
+        (1, 'bytes')
+    )
+    if bytes == 1:
+        return '1 byte'
+    for factor, suffix in abbrevs:
+        if bytes >= factor:
+            break
+    return '%.*f %s' % (precision, bytes / factor, suffix)
+
 def home(request):
     time_took = None
+    size = None
     if request.POST:
         form = UploadForm(request.POST)
         if form.is_valid():
             time_took = form.cleaned_data['time']
+            size = form.cleaned_data['size']
+            size = humanize_bytes(long(size))
     else:
         form = UploadForm()
 
-    return render(request, "welcome.html", { "form": form, "time_took": time_took})
+    return render(request, "welcome.html", { "form": form, "time_took": time_took, "size": size})
 
 
 import json
