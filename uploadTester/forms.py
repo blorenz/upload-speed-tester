74cbc5fffb5c0b8f47c313e939263be983312e78
Blorenz <blorenz@gmail.com>
2014-03-17 09:32:59 -0400

    Hidden input

--- uploadTester/forms.py
+++ uploadTester/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class UploadForm(forms.Form):
-    time = forms.CharField(max_length=20)
-    size = forms.CharField(max_length=20)
+    time = forms.CharField(max_length=20, widget=forms.HiddenInput())
+    size = forms.CharField(max_length=20, widget=forms.HiddenInput())
